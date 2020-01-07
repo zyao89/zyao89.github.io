0dad48898be3788556b570a4552fb5a109b4966c
Zyao89 <zyao89@gmail.com>
2020-01-08 01:23:47 +0800

    :feat: 重构博客功能等。

---
+++ README.md
@@ -0,0 +1,23 @@
+---
+home: true
+# banner: /banner-blog.jpg
+heroText: 'STAY HUNGRY STAY FOOLISH'
+tagline: '这家伙真的很懒。'
+# features:
+#     - image: /banner/designer_mindset.svg
+#     - title: 简洁至上
+#       details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+#       image: /banner/portfolio_update.svg
+#     - title: Vue驱动
+#       details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+#       image: /banner/superhero.svg
+#     - title: 高性能
+#       details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+#       image: /banner/good_team.svg
+footer: true
+---
+
+
+# zyao89.github.io
+
+[Website]
---
+++ micro-app.vuepress.config.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = {
+    type: 'blog',
+    // logo: '/logo.png',
+    // sourceDir: 'docs',
+
+    lang: 'zh-CN',
+    title: "Zyao89's Blogs",
+    description: '这家伙真的很懒。擅长Web前端、Android开发。专业程序员一枚。',
+    head: [
+        [ 'link', { rel: 'icon', href: '/favicon.png' }],
+        [ 'link', { rel: 'manifest', href: '/manifest.json' }],
+        [ 'meta', { name: 'keywords', content: 'Zyao89,Zyao89 Blogs,Android,移动开发,手机APP,javascript,html5,开发者,程序猿,极客,编程,代码,开源,IT网站,Developer,Programmer,Coder,Geek' }],
+        [ 'meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        [ 'meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ],
+    author: 'Zyao89',
+
+    lastUpdated: '上次编辑时间',
+
+    sidebar: 'auto',
+    nav: getNav(),
+
+    sidebarDepth: 2,
+    // algolia: {
+    //     indexName: "cli_vuejs",
+    //     apiKey: "f6df220f7d246aff64a56300b7f19f21"
+    // },
+    // search: false,
+    // searchMaxSuggestions: 10,
+    // displayAllHeaders: true // 默认值：false
+
+    svgIconsDir: 'icons',
+    copyright: '2019 - present, Zyao89',
+    footer: {
+        powerby: true,
+        copyright: true,
+        // beian: 'abc',
+    },
+    // friendLinks: [
+    //     {
+    //         title: 'abc',
+    //         logo: '/logo.png',
+    //         link: 'http://www.baidu.com',
+    //         description: 'abcdsd',
+    //     },
+    // ],
+
+    comment: {
+        type: 'vssue', // default: vssue
+        owner: 'zyao89',
+        repo: 'zyao89.github.io',
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+
+    deploy: {
+        repo: 'git@github.com:zyao89/zyao89.github.io.git',
+        branch: 'gh-pages',
+    },
+};
+
+function getNav() {
+    return [
+        {
+            text: '首页',
+            link: '/',
+            icon: 'home',
+        },
+        {
+            text: '分类',
+            link: '/categories/',
+            icon: 'category',
+            role: 'category', // 只有 category 有此功能
+        },
+        {
+            text: '标签',
+            link: '/tags/',
+            icon: 'tags',
+        },
+        {
+            text: '归档',
+            link: '/timeline/',
+            icon: 'timeline',
+        },
+        {
+            text: '了解更多',
+            items: [
+                { text: 'NPM', link: 'https://www.npmjs.com/zyao89', icon: 'npm' },
+                { text: 'GitHub', link: 'https://github.com/zyao89', icon: 'github' },
+                { text: 'Email', link: 'mailto:zyao89@gmail.com', icon: 'email' },
+            ],
+        },
+    ];
+}
