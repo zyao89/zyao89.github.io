4701e380a0f6ada6fe6fd4bd65b5e041d442d69c
Zyao89 <zyao89@gmail.com>
2020-01-10 00:30:01 +0800

    :update: change deploy yml

---
+++ micro-app.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const config = {
+    plugins: [
+        '@micro-app/plugin-deploy-command',
+    ],
+};
+
+// 兼容两种启动方式
+if (!process.env.MICRO_APP_VUEPRESS_DIRECT_RUNNING) {
+    config.plugins.push('@micro-app/plugin-vuepress');
+}
+
+module.exports = config;
---
+++ micro-app.deploy.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    repo: 'git@github.com:zyao89/zyao89.github.io.git',
+    branch: 'master',
+    dist: '.vuepress/dist',
+    // message: 'AUTO TEST',
+    // cname: 'zyao89.cn',
+};
