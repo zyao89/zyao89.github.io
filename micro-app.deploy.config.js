2df689f92a0be538d8830df8f30d47bad140fa26
Zyao89 <zyao89@gmail.com>
2020-01-10 00:34:12 +0800

    :update: change deploy config

--- micro-app.deploy.config.js
+++ micro-app.deploy.config.js
@@ -4,6 +4,10 @@ module.exports = {
     repo: 'git@github.com:zyao89/zyao89.github.io.git',
     branch: 'master',
     dist: '.vuepress/dist',
+    user: {
+        name: 'Zyao89',
+        email: 'zyao89@gmail.com',
+    },
     // message: 'AUTO TEST',
     // cname: 'zyao89.cn',
 };
